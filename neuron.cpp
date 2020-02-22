4a4046856c8b38511e556a08c44cb43b1d76f22b
kirill.gora <45240428+Elkirillo@users.noreply.github.com>
2020-02-22 17:49:01 +0300

    Update neuron.cpp

--- neuron.cpp
+++ neuron.cpp
@@ -4,97 +4,86 @@
 using namespace std;
 
 
-class neuron {
+class neuron 
+{
 	private:
-	int weights[4];
-	int activation=0;
-	int error;
-	int indata[8][4] {{0,0,0,0},{0,0,0,1},{0,1,0,0},{0,1,0,1},{0,1,1,0},{1,0,0,1},{1,0,1,0},{1,0,1,1}};
-	int delt=0;
-	public:
-	
-
-	
-	//функция заполненя начальными весами 
-	void fullweight (){
-		for (int i=0; i<4; i++){
-		weights[i] = 0;
-		cout << weights[i] << endl;
+		int weights[4];
+		int activation=0;
+		int error;
+		int indata[8][4] {{0,0,0,0},{0,0,0,1},{0,1,0,0},{0,1,0,1},{0,1,1,0},{1,0,0,1},{1,0,1,0},{1,0,1,1}};
+		int delt=0;
+	public:	
+		//функция заполненя начальными весами 
+		void fullweight ()
+		{
+			for (int i=0; i<4; i++)
+			{
+				weights[i] = 0;
+				cout << weights[i] << endl;
+			}	
+		}	
+		//функция активации
+		void activ(int n)
+		{
+			activation=0;
+			for (int i=0;i<4; i++)
+				activation=activation+weights[i]*indata[n][i];
+			cout << "the first result= "<<activation << endl;
 		}
-		
-		}
-		
-	//функция активации
-	void activ(int n){
-		activation=0;
-		for (int i=0;i<4; i++)
-		activation=activation+weights[i]*indata[n][i];
-		cout << "the first result= "<<activation << endl;
-	}
-	
-	
-	//функция отклонения
-	void deviation(int expect){
-		if ((activation-expect)!=0)
-		error= abs(activation - expect);
-		
-	}
-	
+		//функция отклонения
+		void deviation(int expect)
+		{
+			if ((activation-expect)!=0)
+				error= abs(activation - expect);
 
-	//обновление весов с помощью обучения методом "Хебба"
-	void foo4 (int expct,int n) {
-	int num=0;
-		while (activation!=expct){
-		activation=0;
-		for (int i=0;i<4;i++){
-			if (weights[i]==0)
-			weights[i]=weights[i]+indata[n][i]*expct;
-			activation=activation+weights[i]*indata[n][i];
-			
-			num=weights[i]*indata[n][i]+weights[i+1]*indata[n][i+1];
-			
-			if (activation > expct){
-			while (activation != expct){
-				 int numb = 0;
-				weights[i]=weights[i]-1;
-				for (int i=0;i<4;i++){
-				numb=numb+weights[i]*indata[n][i];
+		}
+		//обновление весов с помощью обучения методом "Хебба"
+		void foo4 (int expct,int n) 
+		{
+			int num=0;
+			while (activation!=expct)
+			{
+				activation=0;
+				for (int i=0;i<4;i++)
+				{
+					if (weights[i]==0)
+						weights[i]=weights[i]+indata[n][i]*expct;
+					activation=activation+weights[i]*indata[n][i];
+					num=weights[i]*indata[n][i]+weights[i+1]*indata[n][i+1];
+					if (activation > expct)
+					{
+						while (activation != expct)
+						{
+							int numb = 0;
+							weights[i]=weights[i]-1;
+							for (int i=0;i<4;i++){
+							numb=numb+weights[i]*indata[n][i];
+						}
+						activation=numb;
+					}
 				}
-			activation=numb;
+				if (i!=3)
+				{
+					for (int j=i+1; j<4; j++)
+					{
+						num=weights[i]*indata[n][i]+weights[j]*indata[n][j];
+						if (num > expct)
+							weights[i]=abs(weights[i]-1);
+					}
+					//промежуточный вывод веса
+					cout<<weights[i] << endl;
+				}
 			}
 		}
-		if (i!=3){
-		for (int j=i+1; j<4; j++){
-			num=weights[i]*indata[n][i]+weights[j]*indata[n][j];
-			if (num > expct)
-			weights[i]=abs(weights[i]-1);
-		}
-		
-			
-			cout<<weights[i] << endl;
-		}
 	}
-		
-	
-		
-	}
-}
-	void show() {
+	//функция вывода весов
+	void show() 
+	{
 		for (int i=0;i<4;i++)
 			cout << "weights" << i << "=" <<weights[i]<< endl;
-			cout<< "res= " << activation<< endl;
+		cout<< "res= " << activation<< endl;
 	}
-	
-		
-	
-	
 };
-
-
-
-
-
-
 
 int main (){
 	neuron my;
@@ -129,7 +118,5 @@ int main (){
 	my.foo4(6,5);
 	my.show();
 
-
 	return 0;
-	
 }
